1dca4d2df2b9143de080a52cc738556afbefb517
abhi152003 <abhiprajapati011@gmail.com>
2024-07-02 14:19:33 +0530

    changes in dune API

--- src/app/api/get-avs-names/route.ts
+++ src/app/api/get-avs-names/route.ts
@@ -1,41 +1,56 @@
-import { DuneClient } from "@duneanalytics/client-sdk";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(req: NextRequest) {
-    console.log('Received request:', req.method);
-  
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-      return NextResponse.next({
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET',
-          'Access-Control-Allow-Headers': 'Content-Type',
-        },
-      });
-    }   
+  console.log("Received request:", req.method);
 
-    try {
-        const key = process.env.NEXT_PUBLIC_DUNE_KEY
-        if (key) {
-          const options = {method: 'GET', headers: {'X-DUNE-API-KEY': key}};
-          const duneRes = fetch('https://api.dune.com/api/v1/eigenlayer/avs-metadata', options)
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return NextResponse.next({
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
 
-          const resJson = await (await duneRes).json();
-          const data = resJson.result.rows;
-          // console.log(data)
-
-          return NextResponse.json(data, {
-            status: 200,
-            headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            },
-        });
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+  try {
+    const key = process.env.NEXT_PUBLIC_DUNE_KEY;
+    if (!key) {
+      throw new Error("DUNE API key not found");
     }
-    
+
+    const options = {
+      method: "GET",
+      headers: { "X-DUNE-API-KEY": key },
+    };
+
+    const duneRes = await fetch(
+      "https://api.dune.com/api/v1/eigenlayer/avs-metadata",
+      options
+    );
+
+    if (!duneRes.ok) {
+      throw new Error(`HTTP error! status: ${duneRes.status}`);
+    }
+
+    const resJson = await duneRes.json();
+    const data = resJson.result.rows;
+
+    return NextResponse.json(data, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json(
+      { message: "Error fetching data" },
+      { status: 500 }
+    );
+  }
 }
